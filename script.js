d710a405f4b30def033e13175cb3c0c9ed1f4634
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-19 18:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort((a,b)=>(a-b));
+	let ans=0;
+	tem=0;
+	for(let i of arr){
+		tem+=i;
+		ans+=tem;
+	}
+  return ans;
 }
 
 module.exports=mincost;
